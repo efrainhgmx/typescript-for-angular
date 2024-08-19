518b93e668d1f240143c44280f4d15eccbdffb10
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-19 17:16:04 -0600

    Destructring object TS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { setupCounter } from './counter.ts'
 //import './topics/01-basic-types';
 //import './topics/02.object-interface';
 //import './topics/03-functions.ts';
-import './topics/04-homework-types.ts';
+//import './topics/04-homework-types.ts';
+import './topics/05-basic-destructuring.ts';
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
---
+++ src/topics/05-basic-destructuring.ts
@@ -0,0 +1,27 @@
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration: number;
+    song: string;
+    details: Details;
+}
+
+interface Details {
+    author: string;
+    year: number;
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: 'Mess',
+    details: {
+        author: 'Ed Sheeran',
+        year: 2015
+    }
+}
+
+const { song, audioVolume } = audioPlayer;
+const { details: { author } } = audioPlayer;
+console.log('Song: ', song);
+console.log('Adio: ', audioVolume);
+console.log('Author: ', author);
