ffb8509c9306411dc2886cd1c96d010ff62a9667
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-19 17:42:26 -0600

    Function destructuring

---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,41 @@
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Nokia',
+    price: 150.1
+}
+
+
+const tablet: Product = {
+    description: 'iPad',
+    price: 250.0
+}
+
+const shoppinCart: Product[] = [phone, tablet];
+const tax:number  = 0.15;
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+function taxCalculation( options: TaxCalculationOptions ): [number, number]{
+    let total = 0;
+    const { products, tax } = options;
+    products.forEach( ({ price }) => {
+        total += price;
+    });
+
+    return [total, total * tax];
+}
+
+const [total, taxTotal] = taxCalculation({
+    products: shoppinCart,
+    tax
+});
+
+console.log('Total', total);
+console.log('Total Tax', taxTotal);
