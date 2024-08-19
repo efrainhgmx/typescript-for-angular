3b548db7131757b32cff67dceb9bd70289c9965b
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-19 17:04:54 -0600

    Interface homwork

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 //import './topics/01-basic-types';
 //import './topics/02.object-interface';
-import './topics/03-functions.ts'
+//import './topics/03-functions.ts';
+import './topics/04-homework-types.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
